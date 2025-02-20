aa4d050d1bfd47649440809462c10b9a4043a09c
Snipsx17 <ing.uberthhernandez@gmail.com>
2025-02-20 11:45:23 -0500

    Fix: typo on method to get reservations

--- src/Controller/ReservationController.php
+++ src/Controller/ReservationController.php
@@ -37,7 +37,7 @@ class ReservationController extends AbstractController
     #[Route('/download-json', name: 'download_json')]
     public function downloadJson(): Response
     {
-        $reservation = $this->reservationService->getReservation();
+        $reservation = $this->reservationService->getReservationsData();
         $reservationsAsJson = json_encode($reservation, JSON_PRETTY_PRINT);
 
         return new Response($reservationsAsJson, 200, [
