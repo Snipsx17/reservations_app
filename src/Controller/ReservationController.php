e3b28842cf57d1d5ed19be29f44f67edc1c9edf5
Snipsx17 <ing.uberthhernandez@gmail.com>
2025-02-20 11:19:30 -0500

    Feat: create reservation controller

---
+++ src/Controller/ReservationController.php
@@ -0,0 +1,19 @@
+<?php
+namespace App\Controller;
+
+use App\service\ReservationService;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class ReservationController extends AbstractController
+{
+    private $reservationService;
+
+    public function __construct(ReservationService $reservationService)
+    {
+        $this->reservationService = $reservationService;
+    }
+
+}
