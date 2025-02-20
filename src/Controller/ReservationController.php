b606728ac0c5b517ce513a1d2ebea0b89c606c06
Snipsx17 <ing.uberthhernandez@gmail.com>
2025-02-20 11:37:22 -0500

    Feat: add main route

--- src/Controller/ReservationController.php
+++ src/Controller/ReservationController.php
@@ -1,7 +1,7 @@
 <?php
 namespace App\Controller;
 
-use App\service\ReservationService;
+use App\Service\ReservationService;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -14,6 +14,24 @@ class ReservationController extends AbstractController
     public function __construct(ReservationService $reservationService)
     {
         $this->reservationService = $reservationService;
+    }
+
+    #[Route('/', name: 'reservations')]
+    public function index(Request $request): Response
+    {
+        $reservations = $this->reservationService->getReservationsData();
+        $search = $request->query->get('search', '');
+
+        if ($search) {
+            $reservations = array_filter($reservations, function ($reservation) use ($search) {
+                return stripos(implode(' ', $reservation), $search) !== false;
+            });
+        }
+
+        return $this->render('index.html.twig', [
+            'reservations' => $reservations,
+            'search' => $search,
+        ]);
     }
 
 }
